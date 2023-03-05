e282a83ddcbfb0449a08b59b582797ba357260d4
AmirHossein Matloubi <a.h.matloubi@gmail.com>
2023-03-05 20:17:40 +0330

    Initial Commit

---
+++ ClockPicker/ClockPicker.h
@@ -0,0 +1,18 @@
+//
+//  ClockPicker.h
+//  ClockPicker
+//
+//  Created by AmirHossein Matloubi on 12/14/1401 AP.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ClockPicker.
+FOUNDATION_EXPORT double ClockPickerVersionNumber;
+
+//! Project version string for ClockPicker.
+FOUNDATION_EXPORT const unsigned char ClockPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ClockPicker/PublicHeader.h>
+
+
